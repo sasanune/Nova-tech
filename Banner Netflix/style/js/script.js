939915c938ae35135f9f0c2b4ca26239eff164fa
sasanunesw <162514082+sasanune@users.noreply.github.com>
2024-09-17 16:41:11 -0300

    Add files via upload

---
+++ Banner Netflix/style/js/script.js
@@ -0,0 +1,7 @@
+//Seleciona o botão com o ID 'cta-button'
+const ctaButton = document.getElementById('cta-button');
+
+//Adiciona um evento de clique ao botão
+ctaButton.addEventListener('click', function() {
+    alert('Você será um super programador');
+});

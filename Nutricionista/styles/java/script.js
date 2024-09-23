d9149cc37273b458a1c7b2b06f265d2dfa81fb06
sasanunesw <162514082+sasanune@users.noreply.github.com>
2024-09-23 17:54:27 -0300

    Add files via upload

--- Nutricionista/styles/java/script.js
+++ Nutricionista/styles/java/script.js
@@ -1,24 +1,9 @@
-let slideIndex = 0;
-
-function showSlides() {
-    const slides = document.querySelectorAll('.slide');
-    slides.forEach((slide, index) => {
-        slide.style.display = (index === slideIndex) ? 'block' : 'none';
-    });
-}
-
-function changeSlide(n) {
-    const slides = document.querySelectorAll('.slide');
-    slideIndex += n;
-
-    if (slideIndex < 0) {
-        slideIndex = slides.length - 1;
-    } else if (slideIndex >= slides.length) {
-        slideIndex = 0;
-    }
-
-    showSlides();
-}
-
-// Inicializa o carrossel
-showSlides();
+function calculateIMC() {
+    const weight = parseFloat(document.getElementById("weight").value);
+    const height = parseFloat(document.getElementById("height").value);
+    const imc = weight / (height * height);
+    document.getElementById("imcResult").innerText = `Seu IMC é: ${imc.toFixed(
+      2
+    )}`;
+  }
+  
